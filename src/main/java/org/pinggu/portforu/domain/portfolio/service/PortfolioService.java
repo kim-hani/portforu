481eeb565028fbf39c229cfd0904b4170cc95dbb
ChungWon <wjdcjddnjs1234@gmail.com>
2025-04-17 20:01:05 +0900

    ref(#45) : move s3

--- src/main/java/org/pinggu/portforu/domain/portfolio/service/PortfolioService.java
+++ src/main/java/org/pinggu/portforu/domain/portfolio/service/PortfolioService.java
@@ -2,7 +2,6 @@ package org.pinggu.portforu.domain.portfolio.service;
 
 import lombok.RequiredArgsConstructor;
 import org.pinggu.portforu.common.domain.Pagecond;
-import org.pinggu.portforu.domain.portfolio.s3.service.S3Service;
 import org.pinggu.portforu.common.exception.CustomException;
 import org.pinggu.portforu.domain.member.entity.Member;
 import org.pinggu.portforu.domain.member.repository.MemberRepository;
--- src/main/java/org/pinggu/portforu/domain/portfolio/service/S3Service.java
+++ src/main/java/org/pinggu/portforu/domain/portfolio/service/S3Service.java
@@ -1,4 +1,4 @@
-package org.pinggu.portforu.domain.portfolio.s3.service;
+package org.pinggu.portforu.domain.portfolio.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
