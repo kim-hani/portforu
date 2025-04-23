d103c3c9ed251ae9dffd50be944df5703964973b
문정호 <ansdudn2@naver.com>
2025-04-23 14:54:58 +0900

    change security config

--- src/main/java/org/pinggu/portforu/config/SecurityConfig.java
+++ src/main/java/org/pinggu/portforu/config/SecurityConfig.java
@@ -38,8 +38,7 @@ public class SecurityConfig {
                 .rememberMe(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**", "/oauth2/**", "/login/**", "/error",
-                                "/pay/**","/api/v1/payments/success",
-                                "/api/v1/payments/fail").permitAll()
+                                "/pay/**","/api/v1/payments/**","/payments/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
